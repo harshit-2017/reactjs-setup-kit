5a249202e9065f5b5c3957e13c1d0b99715ead30
HARSHIT <harshit@HARSHITs-MacBook-Pro.local>
2020-10-15 20:20:41 +0530

    react js setup with redux, router, hoc, errorboundary and code splitting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { logo } from "./assets/images";
+import { Login, SignUp } from "./container";
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import AppContainerWithErrorBoundary from "../src/hoc/";
+const LoginContainer = AppContainerWithErrorBoundary(Login);
+const SignUpContainer = AppContainerWithErrorBoundary(SignUp);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/login" component={LoginContainer} />
+        <Route path="/signup" component={SignUpContainer} />
+      </Switch>
+    </Router>
   );
 }
 
